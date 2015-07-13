1b316c94bcce4438c2ebc65a4380019b359dad28
hraboviyvadim <hraboviyvadim@gmail.om>
2015-07-13 13:58:31 +0300

    contacts

--- js/common.js
+++ js/common.js
@@ -1,6 +1,84 @@
 head.ready(function() {
 
-	  // accordeon
+	 // popup
+
+    $('.js-popup').on('click', function(){
+        var link = $(this).data('link');
+
+        $('#' + link).fadeIn('fast');
+    });
+
+    $('.js-popup-close').on('click', function(){
+        $(this).parents('.popup').fadeOut('fast');
+    });
+    $('.popup').on('click', function(event){
+        $(this).fadeOut('fast');
+    });
+    $('.popup__in').on('click', function(){
+        event.stopPropagation();
+    }); 
+
+   // map
+
+    if ($('.map').length) {
+        ymaps.ready(function () {
+          var myMap = new ymaps.Map('YMapsID', {
+              center: [55.48504,28.764819],
+              zoom: 17,
+              controls: []
+          });
+          myMap.controls.add('zoomControl');
+          myMap.behaviors.disable('scrollZoom');
+         // Создаем метку с помощью вспомогательного класса.
+            myPlacemark1 = new ymaps.Placemark([55.48504,28.764819], {
+                // Свойства.
+
+                balloonContent: 'ЖД вокзал',
+                hintContent: 'проспект Франциска Скорины, д.21'
+            }, {
+                // Опции.
+                // Стандартная фиолетовая иконка.
+                iconLayout: 'default#image',
+                iconImageSize: [37, 48],
+                iconImageHref: 'img/marker.png'
+
+            });
+
+         myMap.geoObjects.add(myPlacemark1)
+
+
+
+         var myMap2 = new ymaps.Map('YMapsID2', {
+              center: [55.48504,28.764819],
+              zoom: 17,
+              controls: []
+          });
+          myMap2.controls.add('zoomControl');
+          myMap2.behaviors.disable('scrollZoom');
+         // Создаем метку с помощью вспомогательного класса.
+            myPlacemark2 = new ymaps.Placemark([55.485148,28.766418], {
+                // Свойства.
+
+                balloonContent: 'ЖД вокзал',
+                hintContent: 'проспект Франциска Скорины, д.21'
+            }, {
+                // Опции.
+                // Стандартная фиолетовая иконка.
+                iconLayout: 'default#image',
+                iconImageSize: [37, 48],
+                iconImageHref: 'img/marker.png'
+
+            });
+
+         myMap2.geoObjects.add(myPlacemark2)
+
+        });
+
+          
+
+    };  
+
+    // accordeon
 
     $('.js-accordeon').on('click', function(){
         if ($(this).hasClass('is-active')) {
